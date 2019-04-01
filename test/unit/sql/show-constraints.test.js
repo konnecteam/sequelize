5efb2e499d1311b2b27f1c732dd59bb6cb302a7b
jschlosser <jschlosser@kimoce.com>
2019-04-01 15:54:27 +0200

    fix(show-constraints.test):
    modification in order to respect the new queries for oracle, postgres, mysql who now gets the column name  affected by a constraint.

--- test/unit/sql/show-constraints.test.js
+++ test/unit/sql/show-constraints.test.js
@@ -10,9 +10,9 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
     it('naming', () => {
       expectsql(sql.showConstraintsQuery('myTable'), {
         mssql: "EXEC sp_helpconstraint @objname = N'[myTable]';",
-        postgres: 'SELECT constraint_catalog AS "constraintCatalog", constraint_schema AS "constraintSchema", constraint_name AS "constraintName", table_catalog AS "tableCatalog", table_schema AS "tableSchema", table_name AS "tableName", constraint_type AS "constraintType", is_deferrable AS "isDeferrable", initially_deferred AS "initiallyDeferred" from INFORMATION_SCHEMA.table_constraints WHERE table_name=\'myTable\';',
-        mysql: "SELECT CONSTRAINT_CATALOG AS constraintCatalog, CONSTRAINT_NAME AS constraintName, CONSTRAINT_SCHEMA AS constraintSchema, CONSTRAINT_TYPE AS constraintType, TABLE_NAME AS tableName, TABLE_SCHEMA AS tableSchema from INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE table_name='myTable';",
-        oracle: 'SELECT CONSTRAINT_NAME constraint_name FROM user_cons_columns WHERE table_name = \'MYTABLE\'',
+        postgres: 'SELECT tc.constraint_catalog AS "constraintCatalog", tc.constraint_schema AS "constraintSchema", tc.constraint_name AS "constraintName", tc.table_catalog AS "tableCatalog", tc.table_schema AS "tableSchema", tc.table_name AS "tableName", tc.constraint_type AS "constraintType", tc.is_deferrable AS "isDeferrable", tc.initially_deferred AS "initiallyDeferred", kcu.column_name AS "columnName" from INFORMATION_SCHEMA.table_constraints as "tc" join INFORMATION_SCHEMA.key_column_usage as "kcu" on (tc.constraint_name = kcu.constraint_name and tc.table_name = kcu.table_name) WHERE tc.table_name=\'myTable\';',
+        mysql: "SELECT tc.CONSTRAINT_CATALOG AS constraintCatalog, tc.CONSTRAINT_NAME AS constraintName, tc.CONSTRAINT_SCHEMA AS constraintSchema, tc.CONSTRAINT_TYPE AS constraintType, tc.TABLE_NAME AS tableName, tc.TABLE_SCHEMA AS tableSchema, kcu.column_name AS columnName from INFORMATION_SCHEMA.TABLE_CONSTRAINTS as tc join INFORMATION_SCHEMA.key_column_usage as kcu on (tc.constraint_name = kcu.constraint_name and tc.table_name = kcu.table_name) WHERE tc.table_name='myTable';",
+        oracle: 'SELECT constraint_name constraintName, constraint_type constraintType , column_name columnName from user_constraints natural join user_cons_columns where table_name = \'MYTABLE\'',
         default: "SELECT sql FROM sqlite_master WHERE tbl_name='myTable';"
       });
     });
@@ -20,9 +20,9 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
     it('should add constraint_name to where clause if passed in case of mysql', () => {
       expectsql(sql.showConstraintsQuery('myTable', 'myConstraintName'), {
         mssql: "EXEC sp_helpconstraint @objname = N'[myTable]';",
-        postgres: 'SELECT constraint_catalog AS "constraintCatalog", constraint_schema AS "constraintSchema", constraint_name AS "constraintName", table_catalog AS "tableCatalog", table_schema AS "tableSchema", table_name AS "tableName", constraint_type AS "constraintType", is_deferrable AS "isDeferrable", initially_deferred AS "initiallyDeferred" from INFORMATION_SCHEMA.table_constraints WHERE table_name=\'myTable\';',
-        mysql: "SELECT CONSTRAINT_CATALOG AS constraintCatalog, CONSTRAINT_NAME AS constraintName, CONSTRAINT_SCHEMA AS constraintSchema, CONSTRAINT_TYPE AS constraintType, TABLE_NAME AS tableName, TABLE_SCHEMA AS tableSchema from INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE table_name='myTable' AND constraint_name = 'myConstraintName';",
-        oracle: 'SELECT CONSTRAINT_NAME constraint_name FROM user_cons_columns WHERE table_name = \'MYTABLE\'',
+        postgres: 'SELECT tc.constraint_catalog AS "constraintCatalog", tc.constraint_schema AS "constraintSchema", tc.constraint_name AS "constraintName", tc.table_catalog AS "tableCatalog", tc.table_schema AS "tableSchema", tc.table_name AS "tableName", tc.constraint_type AS "constraintType", tc.is_deferrable AS "isDeferrable", tc.initially_deferred AS "initiallyDeferred", kcu.column_name AS "columnName" from INFORMATION_SCHEMA.table_constraints as "tc" join INFORMATION_SCHEMA.key_column_usage as "kcu" on (tc.constraint_name = kcu.constraint_name and tc.table_name = kcu.table_name) WHERE tc.table_name=\'myTable\';',
+        mysql: "SELECT tc.CONSTRAINT_CATALOG AS constraintCatalog, tc.CONSTRAINT_NAME AS constraintName, tc.CONSTRAINT_SCHEMA AS constraintSchema, tc.CONSTRAINT_TYPE AS constraintType, tc.TABLE_NAME AS tableName, tc.TABLE_SCHEMA AS tableSchema, kcu.column_name AS columnName from INFORMATION_SCHEMA.TABLE_CONSTRAINTS as tc join INFORMATION_SCHEMA.key_column_usage as kcu on (tc.constraint_name = kcu.constraint_name and tc.table_name = kcu.table_name) WHERE tc.table_name='myTable' AND tc.constraint_name = 'myConstraintName';",
+        oracle: 'SELECT constraint_name constraintName, constraint_type constraintType , column_name columnName from user_constraints natural join user_cons_columns where table_name = \'MYTABLE\'',
         default: "SELECT sql FROM sqlite_master WHERE tbl_name='myTable' AND sql LIKE '%myConstraintName%';"
       });
     });
